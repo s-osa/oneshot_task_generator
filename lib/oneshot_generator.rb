bc36b02622950078523ba4f9849f8c183aa9e1d8
OSA Shunsuke <shunsuke-osa@cookpad.com>
2018-02-05 09:40:48 +0900

    Implement simple generator

--- lib/oneshot_generator.rb
+++ lib/oneshot_generator.rb
@@ -1,5 +1,18 @@
-require "oneshot_generator/version"
+require 'rails'
+require 'oneshot_generator/version'
 
-module OneshotGenerator
-  # Your code goes here...
+class OneshotGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
+
+  attr_reader :current, :timestamp, :task_name
+
+  def setup
+    @current   = Time.current
+    @timestamp = @current.strftime('%Y%m%d')
+    @task_name = "#{name.underscore}_#{timestamp}"
+  end
+
+  def create_file_with_template
+    template 'oneshot.rake.erb', "lib/tasks/oneshot/#{timestamp}_#{name.underscore}.rake"
+  end
 end
