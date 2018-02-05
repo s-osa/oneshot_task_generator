9f43de8a9cd472f57411462ee3735422dbb17c59
OSA Shunsuke <shunsuke-osa@cookpad.com>
2018-02-05 09:11:55 +0900

    Init oneshot_generator

---
+++ lib/oneshot_generator.rb
@@ -0,0 +1,5 @@
+require "oneshot_generator/version"
+
+module OneshotGenerator
+  # Your code goes here...
+end
---
+++ spec/oneshot_generator_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe OneshotGenerator do
+  it "has a version number" do
+    expect(OneshotGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
