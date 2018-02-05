d7969a0c043f6fddd9fcd0c12bd92d513f1a89ad
OSA Shunsuke <shunsuke-osa@cookpad.com>
2018-02-05 20:36:58 +0900

    Prepare generator spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'bundler/setup'
-require 'oneshot_task_generator'
+require 'rails/all'
+require 'ammeter/init'
+
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
